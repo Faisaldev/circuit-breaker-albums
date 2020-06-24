6ec5a6be3a68060dab17b51aa52aa873a707532b
Faisal Ahmed <faisal.dev@gmail.com>
2020-06-24 11:04:43 +0300

    Initial commit

---
+++ src/main/java/com/aljazira/resilience/circuitbreakeralbums/CircuitBreakerAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.aljazira.resilience.circuitbreakeralbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerAlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitBreakerAlbumsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aljazira/resilience/circuitbreakeralbums/CircuitBreakerAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljazira.resilience.circuitbreakeralbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerAlbumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
